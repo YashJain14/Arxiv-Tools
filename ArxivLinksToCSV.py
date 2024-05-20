5d29a717ee174f6149bbec82f389208d8c176deb
Yash Jain <67069635+YashJain14@users.noreply.github.com>
2024-05-20 22:04:29 +0800

    Update ArxivLinksToCSV.py

--- ArxivLinksToCSV.py
+++ ArxivLinksToCSV.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import re  # Import regular expressions
+import re  
 
 def fetch_paper_details(urls):
     data = []
@@ -12,6 +12,7 @@ def fetch_paper_details(urls):
         title = page.find('h1', class_='title').text.replace('Title:', '').strip()
         authors = page.find('div', class_='authors').text.replace('Authors:', '').strip()
         date_line = page.find('div', class_='dateline').text.strip()
+        abstract = page.find('blockquote', class_='abstract').text.replace('Abstract:', '').strip()
 
         # Extract the first submission date
         match = re.search(r'\[Submitted on (\d{1,2} \w+ \d{4})', date_line)
@@ -24,6 +25,7 @@ def fetch_paper_details(urls):
             'title': title,
             'authors': authors,
             'submit_date': submit_date,
+            'abstract': abstract,
             'paper_link': url
         })
 
@@ -32,15 +34,17 @@ def fetch_paper_details(urls):
 def save_to_csv(papers_data, filename):
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['Title', 'Link', 'Date', 'Authors'])
+        writer.writerow(['Title', 'Link', 'Date', 'Authors', 'Abstract'])
         for paper in papers_data:
-            writer.writerow([paper['title'], paper['paper_link'], paper['submit_date'], paper['authors']])
+            writer.writerow([paper['title'], paper['paper_link'], paper['submit_date'], paper['authors'], paper['abstract']])
 
 # List of arXiv URLs
-arxiv_urls = ['https://arxiv.org/abs/1606.03498',
- 'https://arxiv.org/abs/1706.08500',
- 'https://arxiv.org/abs/1801.01401',
- 'https://arxiv.org/abs/2203.06026']
+arxiv_urls = [
+    'https://arxiv.org/abs/1606.03498',
+    'https://arxiv.org/abs/1706.08500',
+    'https://arxiv.org/abs/1801.01401',
+    'https://arxiv.org/abs/2203.06026'
+]
 
 papers_data = fetch_paper_details(arxiv_urls)
 save_to_csv(papers_data, 'arxiv_papers.csv')
